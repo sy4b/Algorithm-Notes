a8bf7b9f1f300ca72497fe3f329cf26280726ad7
syb <93108481+sy4b@users.noreply.github.com>
2021-11-25 17:52:40 +0800

    Add files via upload

--- Storage/Code/209.长度最小的子数组.cpp
+++ Storage/Code/209.长度最小的子数组.cpp
@@ -3,23 +3,28 @@
  *
  * [209] 长度最小的子数组
  */
-
+/*
+    滑动窗口
+    1. 右边界怎么动？当前和小于target的时候右移
+    2. 左边界怎么动？当前和大于target的时候左移
+*/
 // @lc code=start
 class Solution {
 public:
-    // O(n)解法
     int minSubArrayLen(int target, vector<int>& nums) {
-        int ans=INT_MAX, curr=0;
-        int left=0, right=0;
+        int ans=INT_MAX;
+        int left=0, right=0, sum=0;
         while(right<nums.size()){
-            curr+=nums[right];
-            while(curr>=target){
+            sum+=nums[right];
+            // 左窗口移动
+            while(sum>=target){
                 ans=min(ans, right-left+1);
-                curr-=nums[left];
+                sum-=nums[left];
                 ++left;
             }
             ++right;
         }
+        // 无解时返回0
         return ans==INT_MAX?0:ans;
     }
 };
