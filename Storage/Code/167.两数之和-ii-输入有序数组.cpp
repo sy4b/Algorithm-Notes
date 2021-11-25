a663aa2c5c87eeaee05d472b5b91f2783ca9c0ef
syb <93108481+sy4b@users.noreply.github.com>
2021-11-25 15:52:57 +0800

    Add files via upload

--- Storage/Code/167.两数之和-ii-输入有序数组.cpp
+++ Storage/Code/167.两数之和-ii-输入有序数组.cpp
@@ -5,21 +5,29 @@
  */
 
 // @lc code=start
+/* 
+    双指针解法
+    1. 数组已然有序，所以可以设置初始左右指针为首尾，然后：
+        a. 和大于target，右指针左移
+        b. 和小于target，左指针右移
+    2. 注意这道题的下标从1开始
+*/
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int left=0, right=numbers.size()-1;
+        
         while(left<right){
-            int sum=numbers[left]+numbers[right];
-            if(sum==target){
-                return vector<int>{left+1, right+1};
-            }else if(sum<target){
+            int sum=numbers[right]+numbers[left];
+            if(sum==target) break;
+            else if(sum<target){
                 ++left;
             }else{
                 --right;
             }
         }
-        return {};
+
+        return vector<int>{left+1, right+1};
     }
 };
 // @lc code=end
