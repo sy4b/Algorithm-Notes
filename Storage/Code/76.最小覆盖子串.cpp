dfd261a698aade6a4a3660651b8f05a706c0eaa6
syb <93108481+sy4b@users.noreply.github.com>
2021-11-27 19:52:02 +0800

    Add files via upload

--- Storage/Code/76.最小覆盖子串.cpp
+++ Storage/Code/76.最小覆盖子串.cpp
@@ -8,11 +8,11 @@
 /*
     滑动窗口：
     1. 什么时候移动右边界：当前窗口内的字符串，没有覆盖到子串
-       右边界越出数组边界，说明没有答案
+       右边界越出数组边界，程序可以退出
     2. 什么时候移动左边界：当前窗口的字符串已经覆盖到子串，需要把窗口尽可能缩小
        当继续缩小后不满足题意的时刻，就是满足的一个答案
     3. 多个答案需要比较字符串长度
-    4. 因此需要使用unordered map记录字符个数
+    4. 因此需要使用unordered_map记录字符个数
     5. helper辅助函数，用来比较窗口内是否完全覆盖子串，当窗口某个字符频次小于子串对应字符，false
 */
 class Solution {
@@ -49,22 +49,20 @@ public:
                 if(helper(smap, tmap)){ // 可以移动
                     continue;
                 }else{                  // 不可以移动
-                    --left;
-                    ++smap[s[left]];
                     // 此时退无可退，是答案之一啦
+                    // 答案区间[left-1, right]
                     if(ans==""){        // 还没有答案，直接加入
-                        for(int i=left;i<=right;++i){
+                        for(int i=left-1;i<=right;++i){
                             ans+=s[i];
                         }
                     }else{              // 取长度更小的作为答案
-                        if(ans.size()>right-left+1){
+                        if(ans.size()>right-(left-1)+1){
                             ans="";
-                            for(int i=left;i<=right;++i){
-                            ans+=s[i];
+                            for(int i=left-1;i<=right;++i){
+                                ans+=s[i];
                             }
                         }
                     }
-                    break;              // 记得退出，否则死循环
                 }
             }
             ++right;
